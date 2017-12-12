462a3551d2376698979d62c3270d9a6a04af4de8
RAINROW <1034077418@qq.com>
2017-12-12 21:16:09 +0800

    功能完结

--- src/config/plugins.js
+++ src/config/plugins.js
@@ -3,7 +3,8 @@ import Vue from 'vue';
 // 引用axios
 import axios from 'axios';
 // baseURL 基地址 将来发送请求的时候设置的地址都会拼接到baseURL之后
-axios.defaults.baseURL = 'http://vue.studyit.io/api/';
+// axios.defaults.baseURL = 'http://vue.studyit.io/api/';
+axios.defaults.baseURL = 'http://127.0.0.1:8899/api/';
 
 // 让所有的vue的实例都共享axios
 Vue.prototype.$http = axios;
